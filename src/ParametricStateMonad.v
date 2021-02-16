ee29011fb34b39f8965ecb1d67c072bd6a4702ca
Andrea Tirelli <andreatirelli@Andreas-MacBook-Air.local>
2021-02-16 10:00:22 +0100

    Towards applicative

--- src/ParametricStateMonad.v
+++ src/ParametricStateMonad.v
@@ -25,4 +25,25 @@ Proof. unfold state_comp_map. apply functional_extensionality. intros. unfold co
 destruct (r x). auto. Qed.
 
 
+#[program]
+Instance state_Functor (s : Type) 
+  : Functor (state_comp s ) :=
+  { map := @state_comp_map s  
+  }.
+  
+Next Obligation. apply state_functor_identity. Defined.
+
+Definition state_comp_apply {s} {a b} (f : state_comp s (a -> b)) (fs : state_comp s a)
+  : state_comp s b := 
+  fun s => let (x, s'):= fs s in let (g, s''):= f s' in (g x, s'').
+
+Definition state_comp_pure {s}{a}(x:a): state_comp s a :=
+fun s => (x, s).
+
+Lemma state_applicative_identity {s}{a} (v : state_comp s a):
+  state_comp_apply (state_comp_pure id) v = v.
+Proof. unfold state_comp_apply. apply functional_extensionality. intros. 
+destruct (v x). unfold state_comp_pure. auto. Qed.
+
+
 
