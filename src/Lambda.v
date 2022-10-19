e6f940ab5c0561a7e2a734b95f5414b53d324178
Andrea Tirelli <andrea.tirelli2@studenti.unimi.it>
2022-10-19 10:05:53 +0200

    Big-step now compiles

--- src/Lambda.v
+++ src/Lambda.v
@@ -1,9 +1,20 @@
 Require Import Monads.Tactics.
 Require Import Monads.Utils.
 Require Import Monads.DelayMonad.
-From Coq Require Import List Lia.
+From Coq Require Import List Lia Arith.
 
-Definition OptPartial (A: Type):= Partial(option A).
+CoInductive Computation (A:Type) : Type :=
+| Return : A -> Computation A
+| Bind : forall (B:Type), Computation B-> (B-> Computation A)-> Computation A.
+Arguments Return {A}.
+Arguments Bind {A B}.
+Notation "c >>= f" := (Bind f c) (at level 20).
+Notation "var <- c ; rest" :=
+(Bind c (fun var => rest) )
+(at level 60, right associativity).
+
+
+Definition OptPartial (A: Type):= Partial (option A).
 Definition fail {A: Type}:= rtrn (@None A).
 Print fail.
 
@@ -15,9 +26,8 @@ match x with
 | rtrn None => fail
 | rtrn (Some x) => f x
 | step y => step (bind y f) end.
-Notation "x <- c1 ;; c2" := (bind c1 (fun x => c2)) 
-                             (right associativity, at level 84, c1 at next level).
 
+Definition CompPartial (A: Type) := Computation (option A).
 Parameter const: Type.
 
 Inductive term: Type :=
@@ -31,17 +41,22 @@ Inductive value: Type :=
   | Clos: term -> list value -> value.
 
 Definition env := list value.
+Definition Fail {A: Type}:= Return (@None A).
 
-CoFixpoint bs (t: term) (e:env) : OptPartial(value):=
+
+CoFixpoint bs (t: term) (e:env): (CompPartial value) :=
 match t with 
- | Const i => ret (Int i)
+ | Const i => Return (Some (Int i))
  | Var n => match (nth_error e n) with 
-         | Some v => ret v
-         | None => fail end
- | Fun a => ret (Clos a e) 
- | App a b => v1 <- bs a e ;; v2 <- bs b e ;; match v1 with 
-                                          | Int r => fail
-                                          | Clos g s => bs g (v2::s) end end.
+         | Some v => Return (Some v)
+         | None => Fail end
+ | Fun a => Return (Some (Clos a e)) 
+ | App a b => v1 <- bs a e ; v2 <- bs b e ; match v1 with 
+                                          | None => Fail
+                                          | Some(Int r) => Fail
+                                          | Some(Clos g s) => match v2 with 
+                                                      | None => Fail 
+                                                      | Some z => bs g (z::s) end end end .
 
 
 
