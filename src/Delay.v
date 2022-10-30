86fd12117a4014f799286465b0ffdc5be4efc561
Andrea Tirelli <andreatirelli@Andreas-MacBook.homenet.telecomitalia.it>
2022-10-30 13:54:36 +0100

    Add eq relation properties for Eqp

--- src/Delay.v
+++ src/Delay.v
@@ -50,11 +50,148 @@ Inductive val {A}: Partial A -> A -> Prop :=
 CoInductive Eqp {A}: Partial A -> Partial A -> Prop :=
 | eqp_value : forall (x y:Partial A)(a:A), val x a -> val y a -> (Eqp x y)
 | eqp_step : forall (x y:Partial A), Eqp x y -> Eqp (step x) (step y).
+Notation "^ a " := (rtrn a) (at level 0).
+Notation "|> x" := (step x) (at level 0, right associativity).
 
-Lemma eqp_equal: forall {A} (x: Partial A), Eqp x x. 
+Notation "x ~> a" := (val x a) (at level 71).
+Notation "x ~^" := (Diverge x) (at level 71).
+
+Notation "x [=] y" := (Eqp x y) (at level 70).
+
+Lemma partial_basic_1 : forall {A} (x y:(Partial A))(a:A), x~>a -> y~>a -> x [=] y.
+Proof. intros. apply eqp_value with (a:=a); assumption. Qed.
+
+Lemma partial_basic_2 : forall {A} (x y:(Partial A)), x~^ -> y~^ -> x [=] y.
+Proof.
+cofix H.
+intros  A x y Hx Hy.
+inversion_clear Hx; inversion_clear Hy.
+apply eqp_step.
+apply H; assumption.
+Qed.
+
+Lemma partial_basic_5 : forall {A} (x:(Partial A))(a:A), x~>a -> x[=] ^a.
+Proof.
+intros; apply eqp_value with (a:=a); try assumption.
+apply value_return.
+Qed.
+
+Lemma partial_basic_13 : forall {A} (a1 a2:A), ^a1~>a2 -> a1=a2.
+Proof.
+intros A a1 a2 H; inversion_clear H; trivial.
+Qed.
+
+Lemma partial_basic_6 : forall {A} (x:(Partial A))(a:A), x[=]^a -> x~>a.
+Proof.
+intros A x a H; inversion_clear H.
+rewrite partial_basic_13 with (a1:=a) (a2:=a0); assumption.
+Qed.
+
+Lemma partial_basic_14 : forall {A} (x:Partial A), x [=] x.
+Proof.
+Admitted. 
+
+Lemma eqp_symm : forall {A} (x y:Partial A), x [=] y -> y [=] x.
+Proof.
+Admitted. 
+
+Lemma partial_basic_7 : forall {A} (x:(Partial A))(a:A), |>x~>a -> x~>a.
+Proof.
+intros A x a H; inversion_clear H; assumption.
+Qed.
+
+Lemma partial_basic_11 : forall {A} (x y:Partial A), x [=] y -> x [=] |>y.
+Proof.
+cofix H.
+intros A x y Heq; inversion_clear Heq.
+apply eqp_value with (a:=a); [idtac|apply value_bind]; assumption.
+apply eqp_step; apply H; assumption.
+Qed.
+
+Lemma partial_basic_12 : forall {A} (x y:Partial A), |>x [=] y -> x[=]y.
+Proof.
+intros A x y Heq;inversion_clear Heq.
+apply eqp_value with (a:=a); [apply partial_basic_7|idtac]; assumption.
+apply partial_basic_11; assumption.
+Qed.
+
+Lemma partial_basic_3 : forall {A} (x y:(Partial A))(a:A), x~>a -> x [=] y -> y~>a.
+Proof.
+intros A x y a Hx; elim Hx.
+intros a0 H0; apply partial_basic_6.
+apply eqp_symm; assumption.
+
+intros x0 a0 H0 IH Heq.
+apply IH; apply partial_basic_12; assumption.
+Qed.
+
+(*Lemma partial_basic_8 : forall {A} (x:Partial A), |>x~^ -> x~^.
+Proof.
+intros A x Hx; inversion_clear Hx.
+assumption.
+Qed.
+
+Lemma partial_basic_9 : forall {A} (x:(Partial A))(a:A), x~>a -> ~ x~^.
+Proof.
+intros  A x a H; elim H.
+intros a0 H0; inversion H0.
+
+intros x0 a0 H0 IH H1.
+apply IH; apply partial_basic_8; assumption.
+Qed.
+
+Lemma partial_basic_4 : forall {A} (x y:Partial A), x~^ -> x [=] y -> y~^.
+Proof.
+cofix H.
+intros A x y Hx Heq.
+generalize Hx; inversion_clear Heq.
+elim partial_basic_9 with (x:=x)(a:=a); assumption.
+intro H1; apply diverge.
+apply H with (x:=x0); [apply partial_basic_8|idtac]; assumption.
+Qed.
+
+Lemma partial_basic_10 : forall {A} (x y:(Partial A))(a:A), x~>a -> y~^ -> ~ x[=]y.
+Proof.
+intros A x y a H; elim H.
+intros a0 H0 H1.
+apply partial_basic_9 with (x:=y)(a:=a0).
+apply partial_basic_6; apply eqp_symm; assumption.
+assumption.
+
+intros x0 a0 H0 IH H1 H2; apply IH.
+assumption.
+apply partial_basic_12; assumption.
+Qed.
+*)
+Lemma eqp_trans : forall {A} (x y z:Partial A), x [=] y -> y [=] z -> x [=] z.
+Proof.
+cofix H.
+intros A x y z Hxy; inversion_clear Hxy.
+intro Hyz.
+apply partial_basic_1 with (a:=a); [assumption|idtac].
+apply partial_basic_3 with (x:=y); assumption.
+
+intro Hyz; inversion_clear Hyz.
+apply partial_basic_1 with (a:=a); [idtac|assumption].
+apply partial_basic_3 with (x:=|>y0); [assumption|apply eqp_step].
+apply eqp_symm; assumption.
+
+apply eqp_step; apply H with (y:=y0); assumption.
+Qed.
+
+Lemma eqp_refl: forall {A} (x: Partial A), Eqp x x. 
 Proof. cofix CIH. intros.  destruct x. 
 apply eqp_value with (a:=a); constructor. constructor. apply CIH. Qed. 
 
+Lemma eqp_sym: forall {A} (x y: Partial A), Eqp x y -> Eqp y x. 
+Proof. cofix CIH. intros. inversion H. 
+- apply eqp_value with (a:=a); assumption. 
+- constructor. apply CIH in H0. assumption. Qed.
+
+Lemma val_det: forall {A}  (a b: A) (x: Partial A), val x a -> val x b -> a = b. 
+Proof. intros. induction H. 
+- inversion H0. auto. 
+- inversion H0. apply IHval in H2. assumption. Qed.  
 
 
 Section park_principle.
@@ -73,10 +210,10 @@ intros. destruct m1. destruct m2.
 apply H in H0.
 destruct H0.
 inversion H0. destruct  H1. inversion H1. inversion H2. auto. 
-apply eqp_equal.
+apply eqp_refl.
 destruct H0. inversion H0. inversion H1.
 destruct H2. destruct H3. inversion H2.
-rewrite H0. apply eqp_equal.
+rewrite H0. apply eqp_refl.
 
 apply H in H0. destruct H0. destruct H0. destruct H0. 
 apply eqp_value with (a:=x); assumption.
@@ -88,7 +225,7 @@ apply eqp_value with (a:=x); assumption.
 destruct H0. destruct H0. destruct H0. destruct H0. destruct H1.
 rewrite H0. rewrite H1. constructor. apply CIH. assumption. 
 
-rewrite H0. apply eqp_equal. Qed.
+rewrite H0. apply eqp_refl. Qed.
 End park_principle.
 
 Theorem partial_eqp_frob : forall A (m1 m2 : Partial A),
@@ -97,8 +234,8 @@ Proof. intros. assert (frob m1 = m1). rewrite frob_eq. reflexivity.
 assert (frob m2 = m2). rewrite frob_eq. reflexivity. 
 rewrite <- H0. rewrite <- H1. assumption. Qed.
 
-Ltac eval_ R :=  rewrite frob_eq with (x:=R); simpl; try (apply eqp_equal).
-Ltac finish_with R := try constructor; try (apply eqp_equal); apply R.
+Ltac eval_ R :=  rewrite frob_eq with (x:=R); simpl; try (apply eqp_refl).
+Ltac finish_with R := try constructor; try (apply eqp_refl); apply R.
 
 (* Functor axioms *) 
 Theorem map_id_eq: forall A (m: Partial A), Eqp (map id m) m.
@@ -163,12 +300,12 @@ eval_ (apply (pure g) (step x)). finish_with CIH. Qed.
 Theorem bindleft_identity : forall A B (a : A) (f : A -> Partial B),
   Eqp (bind f (rtrn a)) (f a).
 Proof. 
-intros. eval_ (bind f (rtrn a)). destruct (f a); apply eqp_equal. Qed.
+intros. eval_ (bind f (rtrn a)). destruct (f a); apply eqp_refl. Qed.
 
 Theorem bindright_identity : forall A (m : Partial A),
   Eqp (bind ret m) m.
 Proof. cofix CIH.  intros.   
-eval_ (bind ret m).  destruct m.  apply eqp_equal. finish_with CIH. Qed.
+eval_ (bind ret m).  destruct m.  apply eqp_refl. finish_with CIH. Qed.
 
 Theorem bind_assoc : forall A B C (m : Partial A) 
 (f : A -> Partial B) (g : B -> Partial C),
